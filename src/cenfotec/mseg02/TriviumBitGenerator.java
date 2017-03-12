4f61dd8c3edb7a33a1643f7c035f26f9135ce5f3
Oscar Gutierrez <osquinor@hotmail.com>
2017-03-12 16:17:08 -0600

    Adding constructors and util methods.

--- src/cenfotec/mseg02/TriviumBitGenerator.java
+++ src/cenfotec/mseg02/TriviumBitGenerator.java
@@ -25,5 +25,63 @@ public class TriviumBitGenerator {
     private BooleanRegister regA = new BooleanRegister(REG_A_SIZE, REG_A_FF_OFFSET, REG_A_FB_OFFSET);
     private BooleanRegister regB = new BooleanRegister(REG_B_SIZE, REG_B_FF_OFFSET, REG_B_FB_OFFSET);
     private BooleanRegister regC = new BooleanRegister(REG_C_SIZE, REG_C_FF_OFFSET, REG_C_FB_OFFSET);
+
+    private final int KEY_SIZE = 80;
+    private final int IV_SIZE = 80;
     
+    private Boolean[] keyArray = new Boolean[KEY_SIZE];
+    private Boolean[] ivArray = new Boolean[IV_SIZE];
+    
+    public TriviumBitGenerator(){
+        for(int i = 0; i < KEY_SIZE; ++i ){
+            keyArray[i] = false;
+        }
+        for(int i = 0; i < IV_SIZE; ++i ){
+            ivArray[i] = false;
+        }
+    }
+        
+    public TriviumBitGenerator(Boolean[] key, Boolean[] iv){
+        if(key.length != KEY_SIZE){
+            throw new IllegalArgumentException("Key should have a size of " + KEY_SIZE);
+        }
+        if(iv.length != IV_SIZE){
+            throw new IllegalArgumentException("Initialization vector should have a size of " + IV_SIZE);
+        }
+        
+        this.keyArray = key;
+        this.ivArray = iv;
+    }
+    
+    /**
+     * @return the key
+     */
+    public Boolean[] getKey() {
+        return keyArray;
+    }
+
+    /**
+     * @param keyArray the key to set
+     */
+    public void setKey(Boolean[] keyArray) {
+        this.keyArray = keyArray;
+    }
+
+    /**
+     * @return the ivArray
+     */
+    public Boolean[] getIV() {
+        return ivArray;
+    }
+
+    /**
+     * @param ivArray the iv to set
+     */
+    public void setIV(Boolean[] ivArray) {
+        this.ivArray = ivArray;
+    }
+    
+    public void WarmUpBitGeneratorEngine(){
+    
+    }
 }
